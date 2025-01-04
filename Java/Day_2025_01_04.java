a354f3b2056aa47d50c1d559a71611b8e0c10350
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-01-04 20:30:59 +0530

    Needs to revisit. Solved this with brute-force in Java.

---
+++ Java/Day_2025_01_04.java
@@ -0,0 +1,28 @@
+class Solution {
+    // brute force
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        // This is important because we are giving return statement inside of nested
+        // for.
+        // This return will work <=> There is no valid pair found
+        return new int[] {};
+    }
+}
+// This will be revisited once Map, HasMap, Set is read
+
+public class Day_2025_01_04 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] ans = sol.twoSum(new int[] { 2, 7, 11, 15 }, 9);
+        for (int num : ans) {
+            System.out.println(num);
+        }
+    }
+}
